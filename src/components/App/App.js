00163ef7de4f9f3451cb6afabdc5d6c2df287649
Sanatan <sanatan.iit@gmail.com>
2017-08-05 17:14:44 +0530

    Added a bit more responsiveness

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -112,34 +112,45 @@ const Leaderboard = ({leaderboard, toggle}) =>
   <div className='LeaderBoard'>
     {leaderboard
       .map(({img, username, alltime, recent}, index) =>
-        <a href={`https://www.github.com/${username}`} 
-            target='_blank' 
-            key={index}>
-          <div className='board'>
-            <div 
-              className='leader-image'
-              style = {{
-                backgroundImage: `url(${img})`
-              }}>
-                <div className='image-overlay'></div>
-            </div>
-            <p className='username'>{index + 1}. {username}</p>
-            {toggle
-              ? <div>
-                  <p className='label'>All time Scores</p>
-                  <p className='scores'>{alltime}</p>
-                  <p className='label'>Last 30 Days</p>
-                  <p className='scores'>{recent}</p>
-                </div>
-              : <div>
-                  <p className='label'>Last 30 Days</p>
-                  <p className='scores'>{recent}</p>
-                  <p className='label'>All time Scores</p>
-                  <p className='scores'>{alltime}</p>
-                </div>
-            }
-          </div>
-        </a>
+        <LeaderCard
+          index = {index}
+          img = {img}
+          username = {username}
+          alltime = {alltime}
+          recent = {recent}
+          toggle = {toggle}
+        />
     )}
   </div>
+
+const LeaderCard = 
+  ({index, img, username, alltime, recent, toggle}) => 
+    <a href={`https://www.github.com/${username}`} 
+        target='_blank' 
+        key={index}>
+      <div className='board'>
+        <div 
+          className='leader-image'
+          style = {{
+            backgroundImage: `url(${img})`
+          }}>
+            <div className='image-overlay'></div>
+        </div>
+        <p className='username'>{index + 1}. {username}</p>
+        {toggle
+          ? <div>
+              <p className='label'>All time Scores</p>
+              <p className='scores'>{alltime}</p>
+              <p className='label'>Last 30 Days</p>
+              <p className='scores'>{recent}</p>
+            </div>
+          : <div>
+              <p className='label'>Last 30 Days</p>
+              <p className='scores'>{recent}</p>
+              <p className='label'>All time Scores</p>
+              <p className='scores'>{alltime}</p>
+            </div>
+        }
+      </div>
+    </a>
 export default App
