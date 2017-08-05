bb60f06e071e4e6e6ed4474a5f0dddfed449fb21
Sanatan <sanatan.iit@gmail.com>
2017-08-05 15:27:07 +0530

    Initial Draft of Project || DONE

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,57 +7,130 @@ class App extends Component {
     super(props)
 
     this.state ={
-      toggle: 0
+      toggle: 0,
+      recentLeaders: [],
+      allTimeLeaders: []
     }
+
     this.onToggleClick = this.onToggleClick.bind(this)
     this.fetchUsers = this.fetchUsers.bind(this)
   }
   
-  onToggleClick(event) {
+  componentDidMount() {
     this.fetchUsers()
-    this.setState({toggle: event.target.value})
   }
 
-  fetchUsers(url) {
-    const {toggle} = this.state
-    toggle 
-      ? fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-          .then(response => response.json())
-          .then(result => console.log(result))
-          .then(error => console.log(error))
-      : fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-          .then(response => response.json())
-          .then(result => console.log(result))
-          .then(error => console.log(error))
+  onToggleClick(event) {
+    this.setState({
+      toggle: +event.target.dataset.value
+    })
+  }
+  fetchUsers() {
+    // fetches all_time Leaders and sets it into state
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then(response => response.json())
+      .then(result => this.setState({allTimeLeaders : result}))
+      .then(error => error)
+
+    // fetches last_30_days Leaders and sets it into state
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(response => response.json())
+      .then(result => this.setState({recentLeaders : result}))
+      .then(error => error)
   }
 
   render() { 
-    console.log(this.state.toggle)
+    const {
+      toggle,
+      recentLeaders,
+      allTimeLeaders
+    } = this.state
+    
+    const isDataFetching = 
+      recentLeaders.length === 0 || 
+      allTimeLeaders.length === 0
+    
+    const leaderboard = toggle
+      ? allTimeLeaders
+      : recentLeaders
     return (
       <div className='App'>
-        <Heading />
         <Toggle 
           onToggleClick = {this.onToggleClick}
+          toggle = {toggle}
         />
+        {isDataFetching
+          ? <p>Loading....</p>
+          : <Leaderboard 
+              leaderboard = {leaderboard}
+              toggle = {toggle}
+            />
+        }
       </div>
       )
   }
 }
 
-const Heading = () => 
-  <h1 style={{
-        color : '#999',
-        opacity: '0.7',
-        textAlign: 'center'
-      }}>FCC Leaderboard</h1> 
+const Toggle = ({onToggleClick, toggle}) => {
+  const activeButtonStyle = {
+    backgroundColor: '#20b3e4',
+    color: 'white',
+  }
+  const InactiveButtonStyle = {
+    backgroundColor: 'transparent',
+    color: '#20b3e4',
+  }
+  const styleIfActive1 = (toggle === 0)
+    ? activeButtonStyle
+    : InactiveButtonStyle
 
-const Toggle = ({onToggleClick}) =>
-  <div style={{
-    display: 'flex',
-    justifyContent: 'center',
-  }}>
-    <button onClick = {onToggleClick} value={0}>Last 30 days </button>
-    <button  onClick = {onToggleClick} value={1}>All Time</button>
+  const styleIfActive2 = (toggle === 1)
+    ? activeButtonStyle
+    : InactiveButtonStyle
+
+  return (
+    <div className='Toggle'>
+      <a
+        onClick = {onToggleClick}
+        data-value={0} 
+        style={styleIfActive1}>Last 30 days</a>
+      <a 
+        onClick = {onToggleClick}
+        data-value={1}
+        style={styleIfActive2}>All Time</a>
+    </div>
+  )
+}
+const Leaderboard = ({leaderboard, toggle}) =>
+  <div className='LeaderBoard'>
+    {leaderboard
+      .map(({img, username, alltime, recent}, index) =>
+        <a href={`https://www.github.com/${username}`} target='_blank'>
+          <div className='board' key={index}>
+            <div 
+              className='leader-image'
+              style = {{
+                backgroundImage: `url(${img})`
+              }}>
+                <div className='image-overlay'></div>
+            </div>
+            <p className='username'>{index + 1}. {username}</p>
+            {toggle
+              ? <div>
+                  <p className='label'>All time Scores</p>
+                  <p className='scores'>{alltime}</p>
+                  <p className='label'>Last 30 Days</p>
+                  <p className='scores'>{recent}</p>
+                </div>
+              : <div>
+                  <p className='label'>Last 30 Days</p>
+                  <p className='scores'>{recent}</p>
+                  <p className='label'>All time Scores</p>
+                  <p className='scores'>{alltime}</p>
+                </div>
+            }
+          </div>
+        </a>
+    )}
   </div>
-
 export default App
